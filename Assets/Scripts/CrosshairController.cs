5f9ffe2427c2819adb0d2efa4d3c382364e9be41
D. Suzuki <bboydaisuke@gmail.com>
2021-12-09 22:49:13 +0900

    Initial Commit

---
+++ Assets/Scripts/CrosshairController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 照準をコントロールする
+/// </summary>
+public class CrosshairController : MonoBehaviour
+{
+    /// <summary>照準の画像</summary>
+    [SerializeField] Image _sprite = default;
+    /// <summary>照準のパネル</summary>
+    [SerializeField] RectTransform _crosshair = default;
+    /// <summary>プレイヤー</summary>
+    [SerializeField] PlayerController _player = default;
+    /// <summary>ロックオンしていない時の照準の色</summary>
+    [SerializeField] Color _defaultColor = new Color(0, 0, 0, 0);
+    /// <summary>ロックオンしている時の照準の色</summary>
+    [SerializeField] Color _lockOnColor = Color.red;
+
+    void Update()
+    {
+        if (_player.LockedTarget)
+        {
+            // ロックオンしている時は照準をターゲットに重ねる
+            _crosshair.position = Camera.main.WorldToScreenPoint(_player.LockedTarget.position);
+            _sprite.color = _lockOnColor;
+        }
+        else
+        {
+            _sprite.color = _defaultColor;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using Cinemachine;
+using Photon.Pun;
+using System;
+using System.Linq;
+
+/// <summary>
+/// プレイヤーを制御するコンポーネント
+/// ・通常時は上下左右で動ける
+/// ・敵に近づくとロックオンできる
+/// ・ロックオン中に Fire1 を押すと、対象に向かってダッシュする
+/// ・ダッシュ中は操作できない
+/// ・ダッシュの対象に体当たりできたら、ダッシュ状態は解除されて操作可能な通常状態に戻る
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>仮想カメラの Follow</summary>
+    [SerializeField] Transform _cameraFollowTarget = default;
+    /// <summary>仮想カメラの LookAt</summary>
+    [SerializeField] Transform _cameraLookAtTarget = default;
+    /// <summary>移動する力</summary>
+    [SerializeField] float _movePower = 3;
+    /// <summary>ロックオン可能な距離</summary>
+    [SerializeField] float _lockOnDistance = 5;
+    /// <summary>ダッシュ力</summary>
+    [SerializeField] float _dashPower = 40f;
+    Rigidbody _rb = default;
+    Animator _anim = default;
+    /// <summary>入力された方向の XZ 平面でのベクトル</summary>
+    Vector3 _dir;
+    PhotonView _view = default;
+    /// <summary>ロックオンしているターゲット</summary>
+    Transform _lockedTarget = default;
+    /// <summary>ダッシュ・体当たりの対象となっているターゲット</summary>
+    Transform _dashTarget = default;
+
+    /// <summary>
+    /// ロックオンしているターゲット。何もロックオンしていない時は null
+    /// </summary>
+    public Transform LockedTarget
+    {
+        get { return _lockedTarget; }
+    }
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+        _view = GetComponent<PhotonView>();
+
+        if (_view.IsMine)
+        {
+            SetUpVirtualCamera();
+        }
+    }
+
+    /// <summary>
+    /// 仮想カメラに対して、Follow, LookAt を設定する
+    /// </summary>
+    void SetUpVirtualCamera()
+    {
+        Array.ForEach(GameObject.FindObjectsOfType<CinemachineVirtualCameraBase>(), c =>
+        {
+            c.Follow = _cameraFollowTarget;
+            c.LookAt = _cameraLookAtTarget;
+        });
+    }
+
+    void Update()
+    {
+        // 入力を受け付ける
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        _dir = Vector3.forward * v + Vector3.right * h;
+        // カメラのローカル座標系を基準に dir を変換する
+        _dir = Camera.main.transform.TransformDirection(_dir);
+        // カメラは斜め下に向いているので、Y 軸の値を 0 にして「XZ 平面上のベクトル」にする
+        _dir.y = 0;
+
+        // キャラクターを「現在の（XZ 平面上の）進行方向」に向ける
+        Vector3 xzVelocity = _rb.velocity;
+        xzVelocity.y = 0;
+
+        if (xzVelocity != Vector3.zero)
+        {
+            this.transform.forward = xzVelocity;
+        }
+
+        // ロックオン
+        _lockedTarget = FindLockonTarget();
+
+        if (_lockedTarget && !_dashTarget && Input.GetButtonDown("Fire1"))
+        {
+            _dashTarget = _lockedTarget;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_dashTarget)
+        {
+            // ダッシュ中は操作は利かず、ひたすらターゲットを追いかける
+            Vector3 dir = _dashTarget.position - this.transform.position;
+            dir.y = 0;
+            _rb.AddForce(dir.normalized * _dashPower, ForceMode.Force);
+        }
+        else
+        {
+            // 通常時は操作して動かす
+            _rb.AddForce(_dir.normalized * _movePower, ForceMode.Force);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (_anim)
+        {
+            Vector3 xzVelocity = _rb.velocity;
+            xzVelocity.y = 0;
+            _anim.SetFloat("Speed", xzVelocity.magnitude);
+        }
+    }
+
+    /// <summary>
+    /// ロックオン対象を探す
+    /// </summary>
+    /// <returns>ロックオン対象。それがない場合は null</returns>
+    Transform FindLockonTarget()
+    {
+        var go = GameObject.FindGameObjectsWithTag("Player")
+            .Where(p => Vector3.Distance(this.transform.position, p.transform.position) < _lockOnDistance && !p.Equals(this.gameObject))
+            .OrderBy(p => Vector3.Distance(this.transform.position, p.transform.position)).FirstOrDefault();
+        return go ? go.transform : null;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // 体当たりの対象に体当たりできたら、ターゲットからはずす
+        if (_dashTarget && collision.gameObject.Equals(_dashTarget.gameObject))
+        {
+            _dashTarget = null;
+        }
+    }
+}
